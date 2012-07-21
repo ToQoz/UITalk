1cf009ae6f5fa39d712dfb6045916b47ba65260e
Takatoshi Matsumoto <toqoz403@gmail.com>
2012-07-21 17:27:08 +0900

    gitリポジトリ外の設定ファイルなどの存在チェックのテスト追加

---
+++ spec/deploy/check_config_files_spec.rb
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+require 'spec_helper'
+
+describe :CheckConfigFiles do
+  it {
+    File.exists?("#{Rails.root}/config/initializers/omniauth.rb").should be_true
+  }
+  it {
+    File.exists?("#{Rails.root}/config/initializers/secret_token.rb").should be_true
+  }
+end
