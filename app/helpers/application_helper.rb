1dcced8f751a7fc546a8bf757e7c0bf9544b632e
Takatoshi Matsumoto <toqoz403@gmail.com>
2012-07-21 16:09:05 +0900

    errors_forヘルパー作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def errors_for(object, message=nil)
+    html = ""
+    unless object.errors.blank?
+      html << "<div class='formErrors #{object.class.name.humanize.downcase}Errors'>\n"
+      if message.blank?
+        if object.new_record?
+          html << "\t\t<h5>There was a problem creating the #{object.class.name.humanize.downcase}</h5>\n"
+        else
+          html << "\t\t<h5>There was a problem updating the #{object.class.name.humanize.downcase}</h5>\n"
+        end    
+      else
+        html << "<h5>#{message}</h5>"
+      end  
+      html << "\t\t<ul>\n"
+      object.errors.full_messages.each do |error|
+        html << "\t\t\t<li>#{error}</li>\n"
+      end
+      html << "\t\t</ul>\n"
+      html << "\t</div>\n"
+    end
+    html
+  end  
 end
