5bcdb5cc75ebc93812c6955569b1d6a37ec00ec6
Takatoshi Matsumoto <toqoz403@gmail.com>
2012-07-26 15:00:20 +0900

    UserControllerのSpecにユーザー作成後、ログインしてるかどうかのテスト追加

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,6 +8,7 @@ describe UsersController do
       subject { { :get => "/users" } }
       it { should route_to(controller: "users", action: "index") }
     end
+
     describe "GET /users/:id" do
       subject { { :get => "/users/ToQoz" } }
       it { should route_to(controller: "users", action: "show", id: "ToQoz") }
@@ -23,34 +24,57 @@ describe UsersController do
       it { should route_to(controller: "users", action: "new") }
     end
   end
+
   describe 'UsersController#index' do
-    it '作成済みのuserが@usersにアサインされている。' do
+    it 'は、作成済みのuserを@usersにアサインする。' do
       user = FactoryGirl.create(:user)
       get :index
       assigns(:users).should eq([ user ])
     end
   end
+
   describe 'UsersController#show' do
-    it 'nameで指定されたユーザのインスタンスが@userにアサインされている。' do
+    it 'は、nameで指定されたUserモデルのインスタンスを@userにアサインする。' do
       user = FactoryGirl.create(:user)
       get :show, { id: user.name }
       assigns(:user).should eq(user)
     end
   end
+
+  describe 'UsersController#new' do
+    it 'は、Userモデルにnewメッセージを { name: oauthのニックネーム } というパラメータと一緒に送る。' do
+      session[:oauth_nickname] = "ToQoz"
+      User.should_receive(:new).with(name: "ToQoz")
+      post :new
+      session[:oauth_nickname] = nil
+    end
+    it 'は、Userモデルのインスタンスを@userにassignする。' do
+      post :new
+      assigns(:user).should be_a_new(User)
+    end
+  end
+
   describe 'UsersController#create' do
     before :each do
       @user = FactoryGirl.create :user
     end
-    it 'Userモデルはsaveメソッドを呼び出しのメッセージを受け取る。' do
+    # TODO context
+    it 'は、Userモデルにnewメッセージを送る。' do
       @user.should_receive :save
       User.stub new: @user
       post :create
     end
-    it '作成されたユーザのページにリダイレクトされる。' do
+    it 'は、作成されたユーザのページにリダイレクトする。' do
       @user.should_receive :save
       User.stub new: @user
       post :create
       response.should redirect_to(@user)
+    end
+    it 'は、User#saveが成功したときsession[:user_id]にuserのidをセットする。つまりログインする。' do
+      User.stub new: @user
+      User.stub(:save).and_return(true)
+      post :create
+      session[:user_id].should eq(@user.id)
     end
   end
 end
