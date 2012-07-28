1ee63f569c25fbf4ec35ecaa95570e28adcf7dcc
Satoshi Honda <bin.honda@gmail.com>
2012-07-28 16:26:46 +0900

    コメント登録時のエラー制御追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,33 @@
 class CommentsController < ApplicationController
+  respond_to :html, :json
+
+  # allow from login user
+  before_filter :authenticate_user!, only: [ :create ]
+
   def index
     @comments = Comment.recent(40)
+    respond_with @comments
   end
 
   def show
     @comment = Comment.find(params[:id])
+    respond_with @comment
   end
 
   def create
 
-    # TODO 権限管理みたいなことする
-    @comment = Comment.create({
+    @post = Post.find(params[:post_id])
+
+    @comment = Comment.new({
       body: params[:comment][:body],
       user_id: current_user.id,
       post_id: params[:post_id],
     })
-    redirect_to :back
+    if @comment.save
+      redirect_to @post
+    else
+      render :template => "posts/show", :locals => {:post => @post}
+    end
   end
 
 end
