0a15e08dc0a171e42dd9cabc1d6027e8bfb0cd23
Satoshi Honda <bin.honda@gmail.com>
2012-07-28 18:31:01 +0900

    draper導入

---
+++ app/presenters/presenter.rb
@@ -0,0 +1,18 @@
+class Presenter
+  def self.h(s)
+    CGI.escapeHTML(s)
+  end
+
+  def self.u(s)
+    URI.escape(s)
+  end
+
+  def h(s)
+    CGI.escapeHTML(s)
+  end
+
+  def u(s)
+    URI.escape(s)
+  end
+
+end
