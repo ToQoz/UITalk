de8b9644db3361965b77d3c51e2f068c4f6627d2
Satoshi Honda <bin.honda@gmail.com>
2012-08-04 19:44:13 +0900

    merger

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120721034404) do
+ActiveRecord::Schema.define(:version => 20120804060938) do
 
   create_table "comments", :force => true do |t|
     t.integer  "post_id"
