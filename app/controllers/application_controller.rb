976c2ebd1bf0d358069d6c405218fa6121661589
Takatoshi Matsumoto <toqoz403@gmail.com>
2012-08-04 17:22:39 +0900

    ApplicationController#current_userでの例外処理fix
    Mongoidの送出する例外をキャッチするままだった
    ActiveRecord用に修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   def current_user
     begin
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    rescue Mongoid::Errors::DocumentNotFound
+    rescue ActiveRecord::RecordNotFound
       nil
     end
   end
