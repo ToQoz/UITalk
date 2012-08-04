b2cdbb3344419f1ee6b8a4908695ecdcbb7ce5a9
Satoshi Honda <bin.honda@gmail.com>
2012-08-04 19:06:53 +0900

    バリデーションルールの追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,6 +7,8 @@ class Comment < ActiveRecord::Base
   attr_accessible :available, :body, :post_id, :user_id
 
   validates :body, :presence => true, :length => { :maximum => 1500 }
+  validates :post_id, :presence => true
+  validates :user_id, :presence => true
 
   scope :recent, lambda { |n| order('created_at DESC').limit(n) }
 
