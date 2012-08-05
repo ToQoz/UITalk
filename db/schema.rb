d992ca7904aa19aa7df6eded7558f6877ba04ff6
Satoshi Honda <bin.honda@gmail.com>
2012-08-05 23:56:13 +0900

    commentsテーブルのスキーマ変更

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,9 @@ ActiveRecord::Schema.define(:version => 20120804060938) do
     t.integer  "post_id"
     t.integer  "user_id"
     t.string   "body"
-    t.boolean  "available"
+    t.integer  "good_count", :default => 0
+    t.integer  "bad_count", :default => 0
+    t.boolean  "available", :default => true
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
