bb2fdb67f097a01c9ad23a658093b1e69054a430
Takatoshi Matsumoto <toqoz403@gmail.com>
2012-08-06 23:22:56 +0900

    increase_#{attr}実装

--- spec/models/post_evaluation_spec.rb
+++ spec/models/post_evaluation_spec.rb
@@ -7,4 +7,20 @@ describe PostEvaluation do
     let (:post_evaluation) { FactoryGirl.build(:post_evaluation, { good_count: 3, bad_count: 5 }) }
     it { post_evaluation.total.should eq(-2) }
   end
+
+  describe '#increase_good_count' do
+    let (:post_evaluation) { FactoryGirl.build(:post_evaluation, { good_count: 3, bad_count: 5 }) }
+    before :all do
+      @total = post_evaluation.total
+    end
+    it { post_evaluation.increase_good_count.total.should eq(@total + 1) }
+  end
+
+  describe '#increase_bad_count' do
+    let (:post_evaluation) { FactoryGirl.build(:post_evaluation, { good_count: 3, bad_count: 5 }) }
+    before :all do
+      @total = post_evaluation.total
+    end
+    it { post_evaluation.increase_bad_count.total.should eq(@total - 1) }
+  end
 end
