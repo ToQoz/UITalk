9dd138614ad3e7014caa6acf41f07772269ab188
Takatoshi Matsumoto <toqoz403@gmail.com>
2012-08-06 14:10:59 +0900

    spec内でのファイルの存在チェックPathname(filename).should exist使うよう変更

--- spec/env/check_enough_test_spec.rb
+++ spec/env/check_enough_test_spec.rb
@@ -6,7 +6,8 @@ describe :CheckEnoughTestSpec do
 
   Dir.glob("#{Rails.root}/app/**/**/*.rb") { |f|
     it "#{f}に対応するspecが存在する。" do
-      File.exist?(f.sub(/#{Rails.root}\/app\//, "#{Rails.root}/spec/").sub(/\.rb$/, '_spec.rb')).should be_true
+      filename = f.sub(/#{Rails.root}\/app\//, "#{Rails.root}/spec/").sub(/\.rb$/, '_spec.rb')
+      Pathname(filename).should exist
     end
   }
 end
