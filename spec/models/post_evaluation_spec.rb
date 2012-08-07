93c3d606a4182df29966fbc7d7549fecb4fd8417
Takatoshi Matsumoto <toqoz403@gmail.com>
2012-08-07 22:12:20 +0900

    PostEvaluation#coutableのテスト追加

--- spec/models/post_evaluation_spec.rb
+++ spec/models/post_evaluation_spec.rb
@@ -7,4 +7,10 @@ describe PostEvaluation do
     let (:post_evaluation) { FactoryGirl.build(:post_evaluation, { good_count: 3, bad_count: 5 }) }
     it { post_evaluation.total.should eq(-2) }
   end
+  describe '#countable' do
+    subject { PostEvaluation.new.countable }
+    it { should include('good_count') }
+    it { should include('bad_count') }
+    it { should have(2).items }
+  end
 end
