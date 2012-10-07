6c9bea6744f6b47827c63c9dfe1f53bc2cc7c260
Satoshi Honda <bin.honda@gmail.com>
2012-10-08 01:45:23 +0900

    tagの登録処理追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,9 +14,6 @@ class Post < ActiveRecord::Base
   validates :title, :presence => true, :length => { :maximum => 100 }
   validates :body, :presence => true, :length => { :maximum => 1500 }
 
-  validates :title, :presence => true, :length => { :maximum => 100 }
-  validates :body, :presence => true, :length => { :maximum => 1500 }
-
   scope :recent, lambda { |n| limit(n).order('created_at DESC, id DESC') }
 
   before_save :create_tags
