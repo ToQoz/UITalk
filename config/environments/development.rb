c3a8e78750437b27cc3f371b62caf40c55699987
Takatoshi Matsumoto <toqoz403@gmail.com>
2012-10-18 02:27:43 +0900

    Add zeus

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,12 +34,4 @@ UITalk::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-
-  silence_warnings do
-    begin
-      require 'pry'
-      IRB = Pry
-    rescue LoadError
-    end
-  end
 end
