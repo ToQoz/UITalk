23265617f47200fe672943ae0dfc284cf7a314b9
Satoshi Honda <bin.honda@gmail.com>
2012-10-27 14:59:19 +0900

    create destory action add

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+class CommentsController < ApplicationController
+  respond_to :html, :json
+
+  # allow from login user
+  before_filter :authenticate_user!, only: [ :create,  :destroy ]
+
+  def create
+    @stock = Stock.new({
+      user_id: current_user.id,
+      post_id: params[:post_id],
+    })
+    @stock.save
+    respond_with @stock
+  end
+
+  def destroy
+    @stock = Stock.find(params[:id])
+    @stock.destroy
+    respond_with @stock
+  end
+end
