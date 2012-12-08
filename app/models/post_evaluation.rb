3936bbb5cae58cc4c74b0a6300bb2a750d6274ac
Satoshi Honda <bin.honda@gmail.com>
2012-12-08 18:11:30 +0900

    add post evaluation

--- app/models/post_evaluation.rb
+++ app/models/post_evaluation.rb
@@ -15,8 +15,8 @@ class PostEvaluation < ActiveRecord::Base
   validates :user_id, :presence => true, :uniqueness => { :scope => :post_id, :message => "has already evaluated for this post" }
 
   default_scope where(available: true)
-  scope :good, where(kind: '1')
-  scope :bad, where(kind: '2')
+  scope :good, where(kind: 1)
+  scope :bad, where(kind: 2)
 
   def evaluable_by?(post_id, user_id)
     PostEvaluation.find_by_post_id(post_id).user_id != user_id
